04b24663e871ecf667382ad02fe209b8a8a6f9fc
ZhaoBO <zhaoboy@aliyun.com>
2021-04-05 14:57:15 +0800

    init

---
+++ stream-workbook/src/main/java/com/zhaobo/stream/StreamApplication.java
@@ -0,0 +1,18 @@
+package com.zhaobo.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author: Luck-zb
+ * description：TODO
+ * Date：2021/4/5 - 14:52
+ */
+@SpringBootApplication
+public class StreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApplication.class, args);
+    }
+}
